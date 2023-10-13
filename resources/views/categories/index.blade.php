48e5f6475502659a733954c9129a8de49f81eb27
KoheiShiida <k.shiida141020@icloud.com>
2023-10-14 01:06:50 +0900

    絞り込み機能実装＆修正

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <head>
+        <x-slot name="header">
+            【投稿一覧】
+         </x-slot>
+    </head>
+    <body>
+        <h1>アレンジレシピ掲示板</h1>
+        <a href='/posts/create'>[投稿する]</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class='body'>{{ $post->body }}</p>
+                    <h3>評価点（１００点満点中）</h3>
+                    <p class='review'>{{$post->review}}点/100点</p>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    <div>
+                        <img src="{{ $post->image_url }}" alt="画像が読み込めません。"/>
+                    </div>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">削除する</button> 
+                    </form>
+                </div>
+                @auth
+                <!-- Post.phpに作ったisLikedByメソッドをここで使用 -->
+                @if (!$post->isLikedBy(Auth::user()))
+                    <span class="likes">
+                        <i class="fas fa-heart like-toggle" data-post-id="{{ $post->id }}"></i>
+                    <span class="like-counter">{{$post->likes_count}}</span>
+                    </span><!-- /.likes -->
+                @else
+                    <span class="likes">
+                        <i class="fas fa-heart heart like-toggle liked" data-post-id="{{ $post->id }}"></i>
+                    <span class="like-counter">{{$post->likes_count}}</span>
+                    </span><!-- /.likes -->
+                @endif
+                @endauth
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+        
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
+    </body>
+    </x-app-layout>
